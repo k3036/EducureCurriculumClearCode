80ab6fa4337299819c0ff3465581e07ba73fbf85
K.kosuke <94692670+k3036@users.noreply.github.com>
2022-12-28 11:54:50 +0900

    Add files via upload

---
+++ 2,javascript,jquery/2_4/abc.js
@@ -0,0 +1,10 @@
+function getValue(idname){
+    // value値を取得する
+    var result = document.getElementById(idname).value;
+   
+    // Alertで表示する
+    alert( result );
+  }
+
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>  <script src="abc.js">
